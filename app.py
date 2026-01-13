d06f137e82c92d517fe1aaa857e57540eb3c43f1
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2026-01-13 15:43:51 +0100

    prices

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import io
 import streamlit as st
 import pandas as pd
 import duckdb
@@ -7,7 +8,7 @@ with st.sidebar:
     "Que veux tu réviser  ?",
     ("Joins", "Group By", "Windows Functions"),
     index=None,
-    placeholder="Select contact method...",
+    placeholder="Choisis une option",
 )
 
     st.write("Options sélectionnée :", option)
@@ -16,19 +17,50 @@ st.write("""# Duo SQL App
          Pratiquez le SQL et progresser grâce au système de répétition espacée !
          """)
 
+csv = '''
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+Cappuccino,1.6
+'''
+
+beverages = pd.read_csv(io.StringIO(csv))
+
+csv2 = '''
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+croissant,1.6
+'''
+
+food_items = pd.read_csv(io.StringIO(csv2))
+
+answer = """
+SELECT * FROM beverages
+CROSS JOIN food_items
+"""
 
 
-data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+sql_query = st.text_area("Entrez du texte là", key = "user_input")
+if sql_query:
+    result = duckdb.query(sql_query).df()
+    st.dataframe(result)
 
-df = pd.DataFrame(data)
+tab2, tab3 = st.tabs(["Tables", "Solutions"])
 
-st.slider("Select a number", 0, 100, 50)
+with tab2:
+    st.write("### Beverages Table")
+    st.dataframe(beverages)
 
-st.dataframe(df)
+    st.write("### Food Items Table")
+    st.dataframe(food_items)
 
-sql_query = st.text_area("Entrez du texte ici")
-result = duckdb.query(sql_query).df()
+    st.write("attendus:")
+    st.dataframe(duckdb.query(answer).df())
 
-st.write(f"Vous avez entré : {sql_query}")
-
-st.dataframe(result)
+with tab3:
+    st.write("### Solution")
+    st.code(answer, language='sql')
+    
