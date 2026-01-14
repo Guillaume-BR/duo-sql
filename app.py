a2358f80e4f53fd3b097854b85f3701df7c183fa
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2026-01-14 12:25:37 +0100

    fix typo 2 black formatter app.py

--- app.py
+++ app.py
@@ -5,35 +5,37 @@ import duckdb
 
 with st.sidebar:
     option = st.selectbox(
-    "Que veux tu réviser  ?",
-    ("Joins", "Group By", "Windows Functions"),
-    index=None,
-    placeholder="Choisis une option",
-)
+        "Que veux tu réviser  ?",
+        ("Joins", "Group By", "Windows Functions"),
+        index=None,
+        placeholder="Choisis une option",
+    )
 
     st.write("Options sélectionnée :", option)
 
-st.write("""# Duo SQL App
+st.write(
+    """# Duo SQL App
          Pratiquez le SQL et progresser grâce au système de répétition espacée !
-         """)
+         """
+)
 
-csv = '''
+csv = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
 Cappuccino,1.6
-'''
+"""
 
 beverages = pd.read_csv(io.StringIO(csv))
 
-csv2 = '''
+csv2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
 croissant,1.6
-'''
+"""
 
 food_items = pd.read_csv(io.StringIO(csv2))
 
@@ -43,7 +45,7 @@ CROSS JOIN food_items
 """
 
 
-sql_query = st.text_area("Entrez du texte là", key = "user_input")
+sql_query = st.text_area("Entrez du texte là", key="user_input")
 if sql_query:
     result = duckdb.query(sql_query).df()
     st.dataframe(result)
@@ -62,5 +64,4 @@ with tab2:
 
 with tab3:
     st.write("### Solution")
-    st.code(answer, language='sql')
-    
+    st.code(answer, language="sql")
