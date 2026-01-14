9ef475a3e0575e5b3b06ba407f173b8c0d532cab
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2026-01-14 14:48:29 +0100

    ajout d'une DB avec les exos et un memory state

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
+con = duckdb.connect(database = 'data/sql_exercices_sql.duckdb', read_only = False)
+
 with st.sidebar:
     option = st.selectbox(
         "Que veux tu réviser  ?",
@@ -13,31 +15,15 @@ with st.sidebar:
 
     st.write("Options sélectionnée :", option)
 
+    exercice = con.execute(f"SELECT * FROM memory_state WHERE theme = '{option}'").df()
+    st.write("Exercice du thème sélectionné :")
+    st.dataframe(exercice)
 st.write(
     """# Duo SQL App
          Pratiquez le SQL et progresser grâce au système de répétition espacée !
          """
 )
 
-csv = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-Cappuccino,1.6
-"""
-
-beverages = pd.read_csv(io.StringIO(csv))
-
-csv2 = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-croissant,1.6
-"""
-
-food_items = pd.read_csv(io.StringIO(csv2))
 
 answer = """
 SELECT * FROM beverages
@@ -45,23 +31,24 @@ CROSS JOIN food_items
 """
 
 
-sql_query = st.text_area("Entrez du texte là", key="user_input")
-if sql_query:
-    result = duckdb.query(sql_query).df()
-    st.dataframe(result)
-
-tab2, tab3 = st.tabs(["Tables", "Solutions"])
-
-with tab2:
-    st.write("### Beverages Table")
-    st.dataframe(beverages)
-
-    st.write("### Food Items Table")
-    st.dataframe(food_items)
-
-    st.write("attendus:")
-    st.dataframe(duckdb.query(answer).df())
-
-with tab3:
-    st.write("### Solution")
-    st.code(answer, language="sql")
+#sql_query = st.text_area("Entrez du texte là", key="user_input")
+#if sql_query:
+#    result = duckdb.query(sql_query).df()
+#    st.dataframe(result)
+#
+#tab2, tab3 = st.tabs(["Tables", "Solutions"])
+#
+#with tab2:
+#    st.write("### Beverages Table")
+#    st.dataframe(beverages)
+#
+#    st.write("### Food Items Table")
+#    st.dataframe(food_items)
+#
+#    st.write("attendus:")
+#    st.dataframe(duckdb.query(answer).df())
+#
+#with tab3:
+#    st.write("### Solution") 
+#    st.code(answer, language="sql")
+#
---
+++ init_db.py
@@ -0,0 +1,48 @@
+
+import io
+import pandas as pd
+import duckdb
+
+con = duckdb.connect(database = 'data/sql_exercices_sql.duckdb', read_only = False)
+
+
+#-------------------------------------------
+# EXERCICES LIST
+#-------------------------------------------
+
+data = {
+    "theme": ["Joins", "Windows Functions"],
+    "exercice_name": ["beverages_and_food","simple_windows"],
+    "tables": [["beverages", "food_items"], ["simple_windows_table"]],
+    "last_reviewed": ["2000-01-01", "2000-01-01"]
+}
+
+memory_state_df = pd.DataFrame(data)
+con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
+
+
+#----------------------------------------------
+# CROSS JOIN EXERCISE TABLES
+#----------------------------------------------
+
+csv = """
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+Cappuccino,1.6
+"""
+
+beverages = pd.read_csv(io.StringIO(csv))
+con.execute("CREATE TABLE IF NOT EXISTS beverages AS SELECT * FROM beverages")
+
+csv2 = """
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+croissant,1.6
+"""
+
+food_items = pd.read_csv(io.StringIO(csv2))
+con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
