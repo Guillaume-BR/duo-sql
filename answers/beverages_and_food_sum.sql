04f6eba52fc94cb29619c4b9777d85d4de7698e4
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2026-03-19 12:13:53 +0100

    sql

--- answers/beverages_and_food_sum.sql
+++ answers/beverages_and_food_sum.sql
@@ -2,7 +2,8 @@
 -- consigne: Affiche les combinaisons possibles entre les boissons et les aliments avec le prix total de chaque combinaison remisé de 10%
 -- tables: beverages, food_items
 
-SELECT beverages.beverage, 
+SELECT 
+       beverages.beverage, 
        food_items.food_item, 
        (beverages.price + food_items.price) * 0.9 AS total_price_with_discount 
 FROM beverages 
